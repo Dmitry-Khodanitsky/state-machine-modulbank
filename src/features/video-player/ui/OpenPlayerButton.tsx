7103668d7b53c9c47b21208391a22c14f1ef5696
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2026-01-22 16:19:47 +0300

    refactor: использовать кастомный хук useVideoPlayer для запуска видео

--- src/features/video-player/ui/OpenPlayerButton.tsx
+++ src/features/video-player/ui/OpenPlayerButton.tsx
@@ -1,15 +1,15 @@
 import { Button, Flex } from 'antd'
 import { PlayCircleOutlined } from '@ant-design/icons'
-import { VideoPlayerContext } from '../model/VideoPlayerContext'
+import { useVideoPlayer } from '../lib/useVideoPlayer'
 
 export const OpenPlayerButton: React.FC = () => {
-  const videoPlayerRef = VideoPlayerContext.useActorRef()
+  const { send } = useVideoPlayer()
   return (
     <Flex justify="center" align="center" style={{ minHeight: '100vh' }}>
       <Button
         type="primary"
         size="large"
-        onClick={() => videoPlayerRef.send({ type: 'open' })}
+        onClick={() => send({ type: 'open' })}
       >
         <PlayCircleOutlined style={{ fontSize: '25px' }} />
       </Button>
