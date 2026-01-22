e5b7ae81bf257aaa234cf5b30a2db5d5695d5f3a
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2026-01-22 14:54:01 +0300

    feat: добавить контекст и провайдер для доступа к состоянию без проброса пропсов

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
-const App = () => {
-  return 'Hello'
+import {
+  OpenPlayerButton,
+  VideoPlayer,
+  VideoPlayerProvider,
+} from './features/video-player'
+
+const App: React.FC = () => {
+  return (
+    <VideoPlayerProvider>
+      <OpenPlayerButton />
+      <VideoPlayer />
+    </VideoPlayerProvider>
+  )
 }
 
 export default App
