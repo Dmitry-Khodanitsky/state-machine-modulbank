27ce985ecf2aa845fb0642261545ed7d9f4d81a0
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2026-01-22 15:03:16 +0300

    feat: добавить событие при клике на кнопку
    - При клике на кнопку отправляется событие open и модальное окно с плеером открывается

--- src/features/video-player/ui/OpenPlayerButton.tsx
+++ src/features/video-player/ui/OpenPlayerButton.tsx
@@ -1,10 +1,16 @@
 import { Button, Flex } from 'antd'
 import { PlayCircleOutlined } from '@ant-design/icons'
+import { VideoPlayerContext } from '../model/VideoPlayerContext'
 
 export const OpenPlayerButton: React.FC = () => {
+  const videoPlayerRef = VideoPlayerContext.useActorRef()
   return (
     <Flex justify="center" align="center" style={{ minHeight: '100vh' }}>
-      <Button type="primary" size="large">
+      <Button
+        type="primary"
+        size="large"
+        onClick={() => videoPlayerRef.send({ type: 'open' })}
+      >
         <PlayCircleOutlined style={{ fontSize: '25px' }} />
       </Button>
     </Flex>
